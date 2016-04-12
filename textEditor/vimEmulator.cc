bec8cbd6128d204dd91a97d8d4564e86f67e72e7
baolidakai <baolidakai@gmail.com>
2016-04-11 21:07:33 -0700

    A skeleton of the vim

--- textEditor/vimEmulator.cc
+++ textEditor/vimEmulator.cc
@@ -1,5 +1,9 @@
 /**
  * Simple text editor emulating vim
+ * TODO:
+ * Add more functions including search
+ * substitute
+ * Number
  */
 #include <iostream>
 #include <fstream>
@@ -8,12 +12,12 @@
 #include <cctype>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 #include <cstdio>
 #include <cstdlib>
 #include <unistd.h>
+#include <termios.h>
 using namespace std;
-
-const double WAIT_SECOND = 0.2;
 
 class document {
 public:
@@ -21,10 +25,12 @@ public:
 	document(vector<string>& initialText);
 	document(ifstream& input);
 	void userCommand(char c); // Read from user command, react correspondingly
+	void saveAs(string filename);
 private:
 	vector<string> lines;
 	bool insertMode;
 	int cursorRow, cursorCol;
+	int cursorColMax; // Remember the maximum value encountered
 	bool moveCursorUp(); // Return false if cannot move
 	bool moveCursorDown(); // Return false if cannot move
 	bool moveCursorLeft(); // Return false if cannot move
@@ -32,22 +38,46 @@ private:
 	void moveCursorLeftmost(); // The effect of 0 in vim
 	void moveCursorRightmost(); // The effect of $ in vim
 	void moveForwardWord(); // The effect of w in vim
-	void insert(char c);
+	bool moveCursorNext(); // Move cursor to the next one, if at the end line, move to the next line
+	void moveCursorTop(); // The effect of gg in vim
+	void moveCursorBottom(); // The effect of gg in vim
+	void deleteCurrentLine(); // The effect of dd in vim
 	void printDocument();
+	char currentCharacter();
+	void enterInsertMode(); // The effect of i
+	void insert(char c); // Enter characters in insert mode
+	void newLine(); // Change a newline in insert mode
 };
 
+char getch();
+string redFont(const string& word);
+
 int main() {
+	cout << "\033[1;31mbold red text\033[0m\n" << endl;
 	ifstream input("helloWorld.cc");
 	document doc = document(input);
 	while (true) {
-		cout << "Enter the command: ";
-		string line;
-		getline(cin, line);
-		if (line.size() == 0) {
-			break;
+		char command = getch();
+		if (command != ':') {
+			doc.userCommand(command);
+		} else {
+			cout << ':' << flush;
+			if (getch() == 'w') {
+				cout << 'w' << flush;
+				stringstream ss;
+				while (true) {
+					command = getch();
+					cout << command << flush;
+					ss << command;
+					if (command == '\n') {
+						string filename;
+						ss >> filename;
+						doc.saveAs(filename);
+						return 0;
+					}
+				}
+			}
 		}
-		char command = line[0];
-		doc.userCommand(command);
 	}
 	return 0;
 }
@@ -95,14 +125,52 @@ document::document(ifstream& input) {
 	printDocument();
 }
 
+void document::enterInsertMode() {
+	insertMode = true;
+}
+
+void document::newLine() {
+	string currLine = lines[cursorRow];
+	lines[cursorRow] = cursorCol == 0 ? " " : currLine.substr(0, cursorCol);
+	string nextLine = currLine.substr(cursorCol);
+	lines.insert(lines.begin() + cursorRow + 1, nextLine);
+	++cursorRow;
+	cursorCol = 0;
+	cursorColMax = 0;
+}
+
 void document::insert(char c) {
+	if (c == 27) {
+		// Escape from insert mode
+		insertMode = false;
+	} else if (c == 127 || c == 8) {
+		// Backspace
+		if (cursorCol == 0 && cursorRow != 0) {
+			// Merge with the previous row
+			--cursorRow;
+			cursorCol = lines[cursorRow].size();
+			cursorColMax = cursorCol;
+			lines[cursorRow] += lines[cursorRow + 1];
+			lines.erase(lines.begin() + cursorRow + 1);
+		} else if (cursorCol != 0) {
+			lines[cursorRow].erase(lines[cursorRow].begin() + cursorCol - 1);
+			--cursorCol;
+			--cursorColMax;
+		}
+	} else {
+		lines[cursorRow] = lines[cursorRow].substr(0, cursorCol) + c + lines[cursorRow].substr(cursorCol);
+		++cursorCol;
+		cursorColMax = cursorCol;
+	}
 }
 
 bool document::moveCursorUp() {
 	if (cursorRow != 0) {
 		--cursorRow;
-		if (lines[cursorRow].size() <= cursorCol) {
-			cursorCol = lines.size() - 1;
+		if (lines[cursorRow].size() <= cursorColMax) {
+			cursorCol = lines[cursorRow].size() - 1;
+		} else {
+			cursorCol = cursorColMax;
 		}
 		return true;
 	}
@@ -112,8 +180,10 @@ bool document::moveCursorUp() {
 bool document::moveCursorDown() {
 	if (cursorRow != lines.size() - 1) {
 		++cursorRow;
-		if (lines[cursorRow].size() <= cursorCol) {
-			cursorCol = lines.size() - 1;
+		if (lines[cursorRow].size() <= cursorColMax) {
+			cursorCol = lines[cursorRow].size() - 1;
+		} else {
+			cursorCol = cursorColMax;
 		}
 		return true;
 	}
@@ -121,6 +191,9 @@ bool document::moveCursorDown() {
 }
 
 bool document::moveCursorLeft() {
+	if (cursorColMax != 0) {
+		--cursorColMax;
+	}
 	if (cursorCol != 0) {
 		--cursorCol;
 		return true;
@@ -129,6 +202,7 @@ bool document::moveCursorLeft() {
 }
 
 bool document::moveCursorRight() {
+	++cursorColMax;
 	if (cursorCol != lines[cursorRow].size() - 1) {
 		++cursorCol;
 		return true;
@@ -138,17 +212,72 @@ bool document::moveCursorRight() {
 
 void document::moveCursorLeftmost() {
 	cursorCol = 0;
+	cursorColMax = 0;
 }
 
 void document::moveCursorRightmost() {
 	cursorCol = lines[cursorRow].size() - 1;
+	// Update cursorColMax to the maximum of all possible lengths
+	cursorColMax = max_element(lines.begin(), lines.end(), [] (string s, string t) { return s.size() < t.size(); })->size();
 }
 
 void document::moveForwardWord() {
+	// Go to the first non-empty, non-newline character after first consecutive empty/newline
+	// Step 1: move to the first empty space
+	while (moveCursorNext() && currentCharacter() != ' ') {}
+	// Step 2: move to the next non-empty character
+	while (currentCharacter() == ' ' && moveCursorNext()) {}
+	cursorColMax = cursorCol;
+}
+
+bool document::moveCursorNext() {
+	// If the cursor is not at the end of a line, equivalent to right
+	// If at the end of a line, equivalent to Enter
+	// If at the end of the file, no movement and return false
+	if (cursorRow == lines.size() - 1 && cursorCol == lines[cursorRow].size() - 1) {
+		return false;
+	}
+	if (cursorCol == lines[cursorRow].size() - 1) {
+		++cursorRow;
+		cursorCol = 0;
+		return false;
+	} else {
+		++cursorCol;
+	}
+	return true;
+}
+
+void document::moveCursorTop() {
+	cursorRow = 0;
+	cursorCol = 0;
+	cursorColMax = 0;
+}
+
+void document::moveCursorBottom() {
+	cursorRow = lines.size() - 1;
+	cursorCol = 0;
+	cursorColMax = 0;
+}
+
+char document::currentCharacter() {
+	return lines[cursorRow][cursorCol];
+}
+
+void document::deleteCurrentLine() {
+	cursorCol = 0;
+	cursorColMax = 0;
+	if (cursorRow == lines.size() - 1) {
+		lines.erase(lines.begin() + lines.size() - 1);
+		--cursorRow;
+	} else {
+		lines.erase(lines.begin() + cursorRow);
+	}
+	if (lines.size() == 0) {
+		lines.push_back(" ");
+	}
 }
 
 void document::printDocument() {
-	usleep(WAIT_SECOND * 1000000);
 	system("clear");
 	for (size_t i = 0; i != lines.size(); i++) {
 		if (i != cursorRow) {
@@ -169,6 +298,9 @@ void document::printDocument() {
 void document::userCommand(char c) {
 	if (!insertMode) {
 		switch (c) {
+			case 'i':
+				enterInsertMode();
+				break;
 			case 'h':
 				moveCursorLeft();
 				break;
@@ -190,11 +322,63 @@ void document::userCommand(char c) {
 			case 'w':
 				moveForwardWord();
 				break;
+			case 'g':
+				if (getch() == 'g') {
+					moveCursorTop();
+				}
+				break;
+			case 'd':
+				if (getch() == 'd') {
+					deleteCurrentLine();
+				}
+				break;
+			case 'G':
+				moveCursorBottom();
 			default:
 				cout << "Invalid command!" << endl;
 		}
 	} else {
-		insert(c);
+		if (c != '\n') {
+			insert(c);
+		} else {
+			cout << "newline" << endl;
+			newLine();
+		}
 	}
 	printDocument();
+	if (insertMode) {
+		cout << "Insert Mode" << endl;
+	} else {
+		cout << "Normal Mode" << endl;
+	}
+}
+
+// Credit http://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
+char getch() {
+	char buf = 0;
+	struct termios old = {0};
+	if (tcgetattr(0, &old) < 0) perror("tcsetattr()");
+	old.c_lflag &= ~ICANON;
+	old.c_lflag &= ~ECHO;
+	old.c_cc[VMIN] = 1;
+	old.c_cc[VTIME] = 0;
+	if (tcsetattr(0, TCSANOW, &old) < 0) perror("tcsetattr ICANON");
+	if (read(0, &buf, 1) < 0) perror ("read()");
+	old.c_lflag |= ICANON;
+	old.c_lflag |= ECHO;
+	if (tcsetattr(0, TCSADRAIN, &old) < 0) perror ("tcsetattr ~ICANON");
+	return (buf);
+}
+
+void document::saveAs(string filename) {
+	if (filename.size() == 0) {
+		filename = "tmp";
+	}
+	ofstream output(filename.c_str());
+	copy(lines.begin(), lines.end(), ostream_iterator<string>(output, "\n"));
+	cout << "Saving to " << filename << endl;
+}
+
+string redFont(const string& word) {
+	return "\033[1;31m" + word + "\033[0m";
 }
